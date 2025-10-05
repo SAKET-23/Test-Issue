67e00ba6fe3e5f5eab19bc986128e8be672025a9
Saket Kohinkar <69591071+SAKET-23@users.noreply.github.com>
2025-10-05 17:36:01 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+// WARNING: This file contains a hardcoded secret.
+const config = {
+  apiKey: "sk_live_abcdef1234567890abcdef1234567890", // Hardcoded Stripe-like API key
+  databaseUrl: "mongodb://user:password@host:port/db"
+};
+
+module.exports = config;
+    
